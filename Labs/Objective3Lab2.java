60fc4ac68754b9219b58ba1e159383db5a06d666
Michael Bunn <mlbunn12@gmail.com>
2023-04-13 13:47:14 -0400

    finish objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,12 @@
+public class Objective3Lab2 {
+	public static void main(String [] args){
+		double side1, side2, sum1, hypotenuse;
+
+		side1 = 10;
+		side2 = 8;
+		sum1 = (side1 * side1) + (side2 * side2);
+		hypotenuse = Math.sqrt(sum1);
+	System.out.println(hypotenuse);
+
+	}
+}
