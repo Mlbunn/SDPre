7076d4fc55391c729550c2953fe2803a8b9f4aeb
Michael Bunn <mlbunn12@gmail.com>
2023-04-13 12:39:34 -0400

    finish  objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Michael Bunn";
+		int age = 29;
+		String state = "Iowa";
+
+		System.out.println("Name: "+ name +" Age: " + age + " State: " + state);
+
+	}
+}
